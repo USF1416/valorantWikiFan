7c2cea1ddb8c29329be12febcac7651b70f20860
USF1416 <youssef.hamri.etudiant@gmail.com>
2025-07-25 21:50:20 +0200

    remise a zero

--- src/components/Agents/agents-page.js
+++ src/components/Agents/agents-page.js
@@ -1,20 +1,7 @@
-import RolesComponent from './roles-section/roles-component';
-import AgentsComponent from './agents-section/agents-component';
-import '../../style/AgentsStyle.css';
-
+import "../../style/AgentsStyle.css";
 
 function Agents() {
-  return (
-    <div>
-      <section className="roles-container">
-        <RolesComponent />
-      </section>
-      
-      <section className="agents-container" style={{backgroundImage:'url("/img/valorant-conceptart-contract-screen.png")',backgroundSize:'',backgroundRepeat:'no-repeat'}}>
-        <AgentsComponent />
-      </section>
-    </div>
-  );
+  return <></>;
 }
 
 export default Agents;
--- src/components/Maps/maps-page.js
+++ src/components/Maps/maps-page.js
@@ -1,32 +1,9 @@
-// Maps.js
 import React, { useEffect, useState } from 'react';
-import { fetchValorantData } from '../../api/valorant-unofficial-api';
 import '../../style/MapsStyle.css';
 
 function Maps() {
-  const [maps, setMaps] = useState([]);
-
-  useEffect(() => {
-    const getMaps = async () => {
-      const data = await fetchValorantData('maps');
-      setMaps(data);
-    };
-    getMaps();
-  }, []);
-
   return (
-    <div className="item-container">
-      <h1>Maps</h1>
-      <div className="item-list">
-        {maps.map(map => (
-          <div key={map.uuid} className="item-card">
-            <img src={map.splash} alt={map.displayName} className="item-image" />
-            <h2>{map.displayName}</h2>
-            <p>{map.coordinates}</p>
-          </div>
-        ))}
-      </div>
-    </div>
+    <></>
   );
 };
 
